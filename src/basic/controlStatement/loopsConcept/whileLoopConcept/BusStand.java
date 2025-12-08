b3d8e7dcac1c71d4a2e41e003e8408e52ff312d3
Neha <reshmanasreen30@gmail.com>
2025-12-08 14:18:28 +0530

    feat: origin  all

---
+++ src/basic/controlStatement/loopsConcept/whileLoopConcept/BusStand.java
@@ -0,0 +1,7 @@
+package basic.controlStatement.loopsConcept.whileLoopConcept;
+
+public class BusStand {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/basic/controlStatement/loopsConcept/whileLoopConcept/BusStop.java
@@ -0,0 +1,14 @@
+package basic.controlStatement.loopsConcept.whileLoopConcept;
+
+public class BusStop {
+    public static void main(String[] args) {
+
+        int i=1;
+        while(i<=5){
+            if(i==1 || i==2||i==3 || i==4 || i==5){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}

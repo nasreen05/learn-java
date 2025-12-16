7eb2aea6b2426b5d8cbfecc8b1ca481a7c3b9ea5
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-16 22:50:18 +0530

    feat: Selection  Sort

---
+++ src/basic/array/arrayEasy/SelectionCode.java
@@ -0,0 +1,24 @@
+package basic.array.arrayEasy;
+
+public class SelectionCode {
+    public static void main(String[] args) {
+        int[] num = {2, 9, 5, 3,  4, 1, 8};
+
+        for(int i=0;i< num.length-1;i++){
+
+            for( int j=i+1;j< num.length;j++){
+                if(num[j]<num[i]){
+                    int temp=num[j];
+                    num[j]=num[i];
+                    num[i]=temp;
+
+                }
+            }
+
+        System.out.println("After short");
+        for (int x : num)
+            System.out.println(x);
+
+        }
+    }
+}

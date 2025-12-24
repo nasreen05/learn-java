2bf33e82b4a78f8c74a6597b75b264b9f56b7490
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-24 23:44:25 +0530

    factorial use recursion

---
+++ src/recursion/basic/FactorialNumber.java
@@ -0,0 +1,16 @@
+package recursion.basic;
+
+public class FactorialNumber {
+static int fact(int n){
+    if(n==0)
+        return 1;
+    return  n*fact(n-1);
+}
+
+
+public static void main(String[] args) {
+int number= 5;
+    System.out.println(number+fact(number));
+
+}
+}

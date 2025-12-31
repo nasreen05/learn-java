17861c386b401c96ac7c2ab78d621df4017b73dc
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-01 00:48:28 +0530

    constructor  practice program

---
+++ src/oops/constructor/basicPrograms/Employee1.java
@@ -0,0 +1,19 @@
+package oops.constructor.basicPrograms;
+
+public class Employee1 extends Person1 {
+    private int salary;
+
+    public Employee1() {
+        this(null, 0, 0);
+        System.out.println("Default constructor");
+    }
+
+    public Employee1(String name, int age, int salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+
+    public void display() {
+        System.out.println("name: " + getName() + ", age: " + getAge() + ", salary: " + salary);
+    }
+}

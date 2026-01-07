df97f5aebbafeb19c48ca81d07dcc557532a9dc6
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-07 23:31:53 +0530

    Interface concept example

--- src/oops/interfaceConcept/CalculatorRunner.java
+++ src/oops/interfaceConcept/CalculatorRunner.java
@@ -5,16 +5,31 @@ public class CalculatorRunner {
 
 
 
-    Calculator  calculator= new Calculator();
+    Calculator  calculator= new Calculator() {
+        @Override
+        public void operation(int a, int b) {
+
+        }
+    };
     int result= calculator.add(10, 20);
     System.out.println(result);
     System.out.println(calculator.add(10,20));
 
-    Add add =new Calculator();
+    Add add = new Calculator() {
+        @Override
+        public void operation(int a, int b) {
+
+        }
+    };
     System.out.println(add.add(30,20));
     // System.out.println(add.add(30,20));
 
-    Sub sub= new Calculator();
+    Sub sub= new Calculator() {
+        @Override
+        public void operation(int a, int b) {
+
+        }
+    };
     System.out.println(sub.sub(30,20));
     System.out.println(sub.add(30,20));
 }

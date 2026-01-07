c7572d9c1a815deddb709f362d0f3bcab853db2b
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-07 23:30:51 +0530

    university example

---
+++ src/oops/abstractionConcept/University1.java
@@ -0,0 +1,19 @@
+package oops.abstractionConcept;
+
+public class University1 {
+
+
+        String universityName;
+
+        public University1() {}
+
+        public University1(String universityName) {
+            this.universityName = universityName;
+        }
+
+    // Abstract method
+    public void examination() {
+
+    }
+}
+

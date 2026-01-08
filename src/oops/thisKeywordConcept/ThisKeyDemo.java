c2e4af3437ef7af6380e8bcb691b1d21b339227a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-08 19:56:09 +0530

    thisKeyword Example

--- src/oops/thisKeywordConcept/ThisKeyDemo.java
+++ src/oops/thisKeywordConcept/ThisKeyDemo.java
@@ -5,11 +5,13 @@ class Dog{
     void details(){
         String name="Bull Dog";
         int age=90;
-        System.out.println("this keyword pointing object -");
+        this.name=name;
+        this.age=age;
+        System.out.println("this keyword pointing object -"+this);
         System.out.println("this.name"+this.name);
         System.out.println("this.age"+this.age);
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println("name"+name);
+        System.out.println("age"+age);
     }
 
 }
@@ -17,6 +19,7 @@ public class ThisKeyDemo {
     public static void main(String[] args) {
     Dog d1= new Dog();
     d1.details();
+    System.out.println("===inside main====");
         System.out.println("d1 is pointing object"+d1);
         System.out.println("d1.name"+d1.name);
         System.out.println("d1.age"+d1.age);

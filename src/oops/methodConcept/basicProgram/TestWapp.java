abffdb8fa431db157b22497f2df920ccab1c9676
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-01-18 03:08:54 +0530

    default Method  Example

---
+++ src/oops/methodConcept/basicProgram/TestWapp.java
@@ -0,0 +1,12 @@
+package oops.methodConcept.basicProgram;
+ class WhatsApp{
+     static void welcome(){
+         System.out.println("welcome to whatsapp");
+     }
+ }
+public class TestWapp {
+
+    public static void main(String[] args) {
+        WhatsApp.welcome();
+    }
+ }

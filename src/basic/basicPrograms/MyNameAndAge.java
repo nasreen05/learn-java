e4dcc085ff0b7257a9474f9262bdcd6589f9223b
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-02 23:49:44 +0530

    Example

---
+++ src/basic/basicPrograms/MyNameAndAge.java
@@ -0,0 +1,9 @@
+package basic.basicPrograms;
+
+public class MyNameAndAge {
+
+    public static void main(String[] args) {
+        System.out.println("Neha");
+        System.out.println("21");
+    }
+}
---
+++ src/basic/basicPrograms/StudentDetails.java
@@ -0,0 +1,9 @@
+package basic.basicPrograms;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name="allen";
+        int age=21;
+        String
+    }
+}

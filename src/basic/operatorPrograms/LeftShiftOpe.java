b0a3ae6fff8a0b8740ae5746fe1b4b67f8c1e3e9
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:36:13 +0530

    LeftShift example

---
+++ src/basic/operatorPrograms/LeftShiftOpe.java
@@ -0,0 +1,10 @@
+package basic.operatorPrograms;
+
+public class LeftShiftOpe {
+    public static void main(String[] args) {
+        System.out.println(10<<1);
+        System.out.println(35<<2);
+        System.out.println(55<<2);
+        System.out.println(35<<2);
+    }
+}

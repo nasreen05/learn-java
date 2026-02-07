1a7036ed337cab1a9d7adec09e2135dc3bbcecdb
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:35:41 +0530

    Bitwise XOR example

---
+++ src/basic/operatorPrograms/BitwiseXor.java
@@ -0,0 +1,10 @@
+package basic.operatorPrograms;
+
+public class BitwiseXor {
+    public static void main(String[] args) {
+        System.out.println(10 ^ 20);
+        System.out.println(55 ^ 35);
+        System.out.println(5 ^ 15);
+        System.out.println( 20 ^ 25);
+    }
+}

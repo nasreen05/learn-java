24c418448df4e60555da88b19c4a2eb05bca32fb
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:34:58 +0530

    Bitwise OR example

---
+++ src/basic/operatorPrograms/BitwiseOr.java
@@ -0,0 +1,15 @@
+package basic.operatorPrograms;
+
+public class BitwiseOr {
+    public static void main(String[] args) {
+        System.out.println(10 | 20);
+        System.out.println(55 | 35);
+        System.out.println(15 | 5);
+        System.out.println(20 |25);
+    }
+
+
+
+
+
+}

0f68e60d9ba40eb89b0566eb5d9d1a8c163874ee
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:36:32 +0530

    example

---
+++ src/basic/operatorPrograms/BitwiseComplement.java
@@ -0,0 +1,10 @@
+package basic.operatorPrograms;
+
+public class BitwiseComplement {
+    public static void main(String[] args) {
+        System.out.println(~10);
+        System.out.println(~35);
+        System.out.println(~-25);
+        System.out.println(~-54);
+    }
+}

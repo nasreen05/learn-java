5c035bbde84eb846db21dc60cdcb3a287e1baf23
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:36:55 +0530

    Right Shift example

---
+++ src/basic/operatorPrograms/RightShiftOpe.java
@@ -0,0 +1,10 @@
+package basic.operatorPrograms;
+
+public class RightShiftOpe {
+    public static void main(String[] args) {
+        System.out.println(10>>1);
+        System.out.println(20>>2);
+        System.out.println(30>>1);
+        System.out.println(40>>4);
+    }
+}

a642a7f47f27f8c223ef55a42e93faa5abdbb2d7
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-07 22:34:23 +0530

    Bitwise And example

---
+++ src/basic/operatorPrograms/BitwiseAnd.java
@@ -0,0 +1,10 @@
+package basic.operatorPrograms;
+
+public class BitwiseAnd {
+    public static void main(String[] args) {
+        System.out.println(10 & 20);
+        System.out.println(35 & 30);
+        System.out.println( 55 & 50);
+        System.out.println(20 & 5);
+    }
+}

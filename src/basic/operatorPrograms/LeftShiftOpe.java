ae91f9d533acb9a96a90891209bd006677038f4a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-09 21:56:24 +0530

    Shift example

--- src/basic/operatorPrograms/LeftShiftOpe.java
+++ src/basic/operatorPrograms/LeftShiftOpe.java
@@ -3,8 +3,8 @@ package basic.operatorPrograms;
 public class LeftShiftOpe {
     public static void main(String[] args) {
         System.out.println(10<<1);
-        System.out.println(35<<2);
-        System.out.println(55<<2);
-        System.out.println(35<<2);
+        System.out.println(35<<1);
+        System.out.println(25<<2);
+        System.out.println(5<<4);
     }
 }
--- src/basic/operatorPrograms/RightShiftOpe.java
+++ src/basic/operatorPrograms/RightShiftOpe.java
@@ -3,8 +3,8 @@ package basic.operatorPrograms;
 public class RightShiftOpe {
     public static void main(String[] args) {
         System.out.println(10>>1);
-        System.out.println(20>>2);
-        System.out.println(30>>1);
-        System.out.println(40>>4);
+        System.out.println(35>>2);
+        System.out.println(25>>3);
+        System.out.println(55>>4);
     }
 }

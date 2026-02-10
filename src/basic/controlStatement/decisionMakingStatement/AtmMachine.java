85e73a965636f1e371e80d3c777590e4d06f639b
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-10 18:20:16 +0530

    Atm  Example

---
+++ src/basic/controlStatement/decisionMakingStatement/AtmMachine.java
@@ -0,0 +1,16 @@
+package basic.controlStatement.decisionMakingStatement;
+
+import java.util.Scanner;
+
+public class AtmMachine {
+    public static void main(String[] args) {
+        int pin=1234;
+        double bal=1200000;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Pin :");
+        int upin=sc.nextInt();
+        if(pin==upin){
+
+        }
+    }
+}

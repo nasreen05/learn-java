0d103c06e8c07e05ddad114b58672eb6f015a3af
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-16 21:03:28 +0530

    Example

---
+++ src/basic/controlStatement/jumpingConcept/Words.java
@@ -0,0 +1,11 @@
+package basic.controlStatement.jumpingConcept;
+
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number ");
+
+    }
+}

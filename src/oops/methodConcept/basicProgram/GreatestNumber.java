b4467245f90a025553f883bc193aebfd5de1539a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-20 22:47:44 +0530

    Greatest Number

---
+++ src/oops/methodConcept/basicProgram/GreatestNumber.java
@@ -0,0 +1,18 @@
+package oops.methodConcept.basicProgram;
+
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class GreatestNumber {
+    public static void greater(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  two number : ");
+        int a=sc.nextInt() , b=sc.nextInt();
+        System.out.println(a>b ? a+ " is greater ": b+"is greater");
+    }
+
+    public static void main(String[] args) {
+        greater();
+    }
+}

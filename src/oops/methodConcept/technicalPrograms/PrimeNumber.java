7c7bb4cd49b736c2bc63756e871927e09428423b
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-24 23:49:50 +0530

    Prime Number

---
+++ src/oops/methodConcept/technicalPrograms/PrimeNumber.java
@@ -0,0 +1,23 @@
+package oops.methodConcept.technicalPrograms;
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static  boolean prime(int n) {
+        if (n == 1)
+            return false;
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(" Enter the Number :");
+        int n=sc.nextInt();
+        System.out.println(prime(n)?n+"is a prime ":n+"not a Prime");
+    }
+}

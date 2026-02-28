740105a490e4a9b8158b1948ef81ccba091a5a48
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:30:47 +0530

    long to short

---
+++ src/oops/typeCasting/primitiveTypeCasting/Long_short.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Long_short {
+    public static void main(String[] args) {
+        long l=12l;
+        short s=(short) l;
+        System.out.println(l+" "+s);
+    }
+}

bce53e65fb550d44b7f407ca176da49a3a6041ff
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:29:27 +0530

    int to char

---
+++ src/oops/typeCasting/primitiveTypeCasting/Int_Char.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Int_Char {
+    public static void main(String[] args) {
+        int i=120;
+        char c=(char)i;
+        System.out.println(i+ " "+c);
+    }
+}

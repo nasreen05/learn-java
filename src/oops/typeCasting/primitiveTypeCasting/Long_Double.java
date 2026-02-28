d9537fce9ced0b1c98a6250b000c4ec5723dd707
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:30:32 +0530

    long to  double

---
+++ src/oops/typeCasting/primitiveTypeCasting/Long_Double.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Long_Double {
+    public static void main(String[] args) {
+        long l=50l;
+        double d=(long)l;
+        System.out.println(l+ " "+ d);
+    }
+}

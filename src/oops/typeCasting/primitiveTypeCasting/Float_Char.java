ae3323409e3d4ef145ac1e481934e531c7e1357f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:28:52 +0530

    float to char

---
+++ src/oops/typeCasting/primitiveTypeCasting/Float_Char.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Float_Char {
+    public static void main(String[] args) {
+        float f=3f;
+        char c=(char)f;
+        System.out.println(f + " " + c);
+    }
+}

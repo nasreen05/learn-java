fb2b7a6b2f2afa406348777129fcd46c1743b01d
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:30:02 +0530

    int to long

---
+++ src/oops/typeCasting/primitiveTypeCasting/Int_Long.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Int_Long {
+    public static void main(String[] args) {
+        int i=12;
+        long l=i;
+        System.out.println(i+ " "+l);
+    }
+}

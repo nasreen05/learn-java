ea6c9cd7df9ec0d683e92df0a0ba2836eb61b0c9
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:29:40 +0530

    int to float

---
+++ src/oops/typeCasting/primitiveTypeCasting/Int_Float.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Int_Float {
+    public static void main(String[] args) {
+        int i=56;
+        float f=i;
+        System.out.println( i+ " "+f);
+    }
+}

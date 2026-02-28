096993fd216de5cda1ce038cf91e794ea3d0b363
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:31:54 +0530

    example

---
+++ src/oops/typeCasting/primitiveTypeCasting/Sample.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Sample {
+    public static void main(String[] args) {
+        long l=87l;
+        int i=(int)l;
+        System.out.println(l+" "+i);
+    }
+}

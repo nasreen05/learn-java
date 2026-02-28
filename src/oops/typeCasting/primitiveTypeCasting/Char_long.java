53f95ef9d198881170fe71e8d6cf6aed6163870f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-02-28 10:28:09 +0530

    char to long

---
+++ src/oops/typeCasting/primitiveTypeCasting/Char_long.java
@@ -0,0 +1,9 @@
+package oops.typeCasting.primitiveTypeCasting;
+
+public class Char_long {
+    public static void main(String[] args) {
+        char c= 'A';
+        long l=(char)c;
+        System.out.println(l + " "+c);
+    }
+}

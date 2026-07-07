b10bb8e844df11941f6a5a462af0f44600e8bad4
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-07 06:30:24 +0530

    Example

--- src/oops/classConcept/ClassMain3.java
+++ src/oops/classConcept/ClassMain3.java
@@ -1,12 +1,11 @@
 package oops.classConcept;
 class Main{
-    int no=20;
-    String name="Rehsma";
 
 
-}
-public class ClassMain3 {
     public static void main(String[] args) {
-
+        
     }
 }
+
+
+

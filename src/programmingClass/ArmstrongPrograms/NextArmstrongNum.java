1fb8a9a4e7c52c38787a1553c44c40e81cefc61a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 16:28:20 +0530

    Next Armstrong Number

---
+++ src/programmingClass/ArmstrongPrograms/NextArmstrongNum.java
@@ -0,0 +1,45 @@
+package programmingClass.ArmstrongPrograms;
+
+public class NextArmstrongNum {
+        public static void main(String[] args) {
+            System.out.println(nextArmStrong(700));
+        }
+
+        static int nextArmStrong(int n) {
+            while (true) {
+                n++;
+                if (isArmStrongNumber(n))
+                    return n;
+            }
+        }
+
+        private static boolean isArmStrongNumber(int n) {
+            int count = digitsInNumber(n);
+            int originalNum = n;
+            int sum = 0;
+
+            while (n != 0) {
+                int dig = n % 10;
+                sum += raiseToPower(dig, count);
+                n /= 10;
+            }
+
+            return originalNum == sum;
+        }
+
+        private static int raiseToPower(int dig, int count) {
+            int pow = 1;
+            for (int i = 1; i <= count; i++)
+                pow *= dig;
+            return pow;
+        }
+
+        private static int digitsInNumber(int n) {
+            int count = 0;
+            while (n != 0) {
+                count++;
+                n /= 10;
+            }
+            return count;
+        }
+    }

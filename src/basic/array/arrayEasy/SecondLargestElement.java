4f1b755338fe7a7754e023171ed0e196a80f189c
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 15:57:51 +0530

    Second Leargest Element

--- src/basic/array/arrayEasy/SecondLargestElement.java
+++ src/basic/array/arrayEasy/SecondLargestElement.java
@@ -2,20 +2,20 @@ package basic.array.arrayEasy;
 
 public class SecondLargestElement {
        public static void main(String[] args) {
-            int[] arr = {10, 5, 20, 15, 8};
+           int[] arr = {10, 5, 20, 15, 8};
 
-            int max = Integer.MIN_VALUE;
-            int second = Integer.MIN_VALUE;
+           int max = Integer.MIN_VALUE;
+           int second = Integer.MIN_VALUE;
 
-            for (int x : arr) {
-                if (x > max) {
-                    second = max;
-                    max = x;
-                } else if (x > second && x != max) {
-                    second = x;
-                }
-            }
+           for (int x : arr) {
+               if (x > max) {
+                   second = max;
+                   max = x;
+               } else if (x > second && x != max) {
+                   second = x;
+               }
+           }
 
-            System.out.println("Second Largest = " + second);
-        }
-    }
+           System.out.println("Second Largest = " + second);
+       }
+}

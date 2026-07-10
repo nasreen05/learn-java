5dbcd39322db45c185c847fb869a7dd96e73673f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 16:06:54 +0530

    Right Shift All Element

---
+++ src/basic/array/qspeiderArray/RightShiftElement.java
@@ -0,0 +1,16 @@
+package basic.array.qspeiderArray;
+
+public class RightShiftElement {
+    public static void main(String[] args) {
+        int []a={1,2,3,4,5,6};
+        int temp=a[a.length-1];
+        for(int i=a.length-1;i>0;i--){
+            a[i]=a[i-1];
+            a[0]=temp;
+
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}

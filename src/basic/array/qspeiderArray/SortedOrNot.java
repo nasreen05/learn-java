14da376867fd6e7408a5029bfd5fa06d58871c7a
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 16:12:22 +0530

    Sorted or Not

---
+++ src/basic/array/qspeiderArray/SortedOrNot.java
@@ -0,0 +1,29 @@
+package basic.array.qspeiderArray;
+
+
+import java.security.IdentityScope;
+
+public class SortedOrNot {
+    public static void main(String[] args) {
+        int arr[]={11,8,5,6,9};
+        int asc = 0;
+        int dec = 0;
+        int eq = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] < arr[i + 1]) {
+                asc++;
+            } else if (arr[i] > arr[i + 1]) {
+                dec++;
+            } else {
+                eq++;
+            }
+        }
+        if (eq + asc == arr.length - 1)
+            System.out.println("Ascending Order ");
+        else if (eq + dec == arr.length - 1)
+            System.out.println("Descending Order ");
+        else
+            System.out.println("Not Sorted");
+    }
+}

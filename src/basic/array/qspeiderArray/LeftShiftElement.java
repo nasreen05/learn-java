360bce68f585e32541a1a2c33565e94ffa9a4ba2
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 16:02:42 +0530

    Left Shift All Element

---
+++ src/basic/array/qspeiderArray/LeftShiftElement.java
@@ -0,0 +1,16 @@
+package basic.array.qspeiderArray;
+
+public class LeftShiftElement {
+    public static void main(String[] args) {
+        int []a={1,2,3,4,5};
+        int temp=a[0];
+        for( int i=0;i<a.length-1;i++){
+            a[i]=a[i+1];
+
+        }
+        a[a.length-1]=temp;
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}

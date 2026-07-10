87016e161fb4494183188b6cb4ae2bf2ebe001b0
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-10 16:25:49 +0530

    Count Armstrong Numbers in Range

---
+++ src/programmingClass/ArmstrongPrograms/CountArmNumRange.java
@@ -0,0 +1,22 @@
+package programmingClass.ArmstrongPrograms;
+
+import static programmingClass.ArmstrongPrograms.CheckArmStrong.isArmStrongNumber;
+
+public class CountArmNumRange {
+    public static void main(String[] args) {
+        countOfArmStrongInRange(100, 500);
+    }
+
+    private static void countOfArmStrongInRange(int start, int end) {
+        int count = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isArmStrongNumber(i))
+                count++;
+        }
+
+        System.out.println(count);
+    }
+
+    // Same helper methods
+}

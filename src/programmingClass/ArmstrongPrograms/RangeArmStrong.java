78e7764ac4bef90fdcac5bd44cbb457face185f4
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-13 16:23:47 +0530

    Range Armstrong Number

---
+++ src/programmingClass/ArmstrongPrograms/RangeArmStrong.java
@@ -0,0 +1,18 @@
+package programmingClass.ArmstrongPrograms;
+
+import static programmingClass.ArmstrongPrograms.CheckArmStrong.isArmStrongNumber;
+
+public class RangeArmStrong {
+    public static void main(String[] args) {
+        rangeOfArmStrong(100, 500);
+    }
+
+    private static void rangeOfArmStrong(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isArmStrongNumber(i))
+                System.out.print(i + " ");
+        }
+    }
+
+    // Same helper methods
+}

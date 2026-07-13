65f6994b7e9557e8b678b1c9e20560f775704f3f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-13 16:28:54 +0530

    Smallest Armstrong Number

---
+++ src/programmingClass/ArmstrongPrograms/SmallestArmNumRange.java
@@ -0,0 +1,21 @@
+package programmingClass.ArmstrongPrograms;
+
+import static programmingClass.ArmstrongPrograms.CheckArmStrong.isArmStrongNumber;
+
+public class SmallestArmNumRange {
+    public static void main(String[] args) {
+        System.out.println(smallestArmStrongNumInRange(100, 500));
+    }
+
+    private static int smallestArmStrongNumInRange(int start, int end) {
+
+        for (int i = start; i <= end; i++) {
+            if (isArmStrongNumber(i))
+                return i;
+        }
+
+        return -1;
+    }
+
+    // Same helper methods
+}

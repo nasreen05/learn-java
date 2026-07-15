3089666878d0f0e5f12b9eac565b3dfa54aa6d0c
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-15 06:34:48 +0530

    Swap Odd Index With Even Index in Array

---
+++ src/qspeiderPrograms/qspeiderArray/SwapOddIndexWithEvenIndex.java
@@ -0,0 +1,28 @@
+package qspeiderPrograms.qspeiderArray;
+
+import java.util.Arrays;
+
+public class SwapOddIndexWithEvenIndex {
+    public static int[] swapping(int[] a) {
+
+        for (int i = 1; i < a.length - 1; i++) {
+
+            if (i % 2 != 0) {
+
+                a[i] += a[i + 1];
+                a[i + 1] = a[i] - a[i + 1];
+                a[i] = a[i] - a[i + 1];
+            }
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 8, 3, 6, 4, 7, 10, 9};
+
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(swapping(a)));
+    }
+}

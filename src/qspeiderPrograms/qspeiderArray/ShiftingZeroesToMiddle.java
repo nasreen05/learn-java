38add6064f5c6cd0336ddd9de6528c8177d1fe3e
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-15 06:34:09 +0530

    Shift all the zeros To Middle in Array

---
+++ src/qspeiderPrograms/qspeiderArray/ShiftingZeroesToMiddle.java
@@ -0,0 +1,51 @@
+package qspeiderPrograms.qspeiderArray;
+
+import java.util.Arrays;
+
+public class ShiftingZeroesToMiddle {
+
+
+        public static int[] shiftZeroesToLast(int[] a) {
+
+            int j = 0;
+
+            for (int i = 0; i <= a.length / 2; i++) {
+
+                if (a[i] != 0)
+                    a[j++] = a[i];
+            }
+
+            while (j <= a.length / 2)
+                a[j++] = 0;
+
+            return a;
+        }
+
+        public static int[] shiftZeroesToFirst(int[] a) {
+
+            int k = a.length - 1;
+
+            for (int i = a.length - 1; i >= a.length / 2 + 1; i--) {
+
+                if (a[i] != 0)
+                    a[k--] = a[i];
+            }
+
+            while (k >= a.length / 2 + 1)
+                a[k--] = 0;
+
+            return a;
+        }
+
+        public static void main(String[] args) {
+
+            int[] a = {1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6};
+
+            System.out.println(Arrays.toString(a));
+
+            a = shiftZeroesToFirst(a);
+            a = shiftZeroesToLast(a);
+
+            System.out.println(Arrays.toString(a));
+        }
+    }

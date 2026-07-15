8c499810122a18f38486a9b3eadcbffed057a065
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-15 06:32:23 +0530

    Decimal Into Hexadecimal Convert

---
+++ src/qspeiderPrograms/qspeiderArray/DecimalIntoHexadecimal.java
@@ -0,0 +1,16 @@
+package qspeiderPrograms.qspeiderArray;
+
+public class DecimalIntoHexadecimal {
+    public static void main(String[] args) {
+        StringBuffer sb= new StringBuffer();
+        char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int num=362;
+        while(num !=0){
+            int rem=num%16;
+            sb.insert(0,ch[rem]);
+            num= num/16;
+        }
+        System.out.println(" Number : " +sb );
+    }
+}
+

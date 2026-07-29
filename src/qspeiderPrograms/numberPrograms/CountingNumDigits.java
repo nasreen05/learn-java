47b7dbc05a091cddf11494204914f89f32fe601f
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-07-30 03:19:37 +0530

    Counting Number of Digits

---
+++ src/qspeiderPrograms/numberPrograms/CountingNumDigits.java
@@ -0,0 +1,9 @@
+package qspeiderPrograms.numberPrograms;
+
+public class CountingNumDigits {
+    public static void main(String[] args) {
+        int num=12345554;
+        int count=0;
+
+    }
+}

7b639ee2820fe1f8720c791699ecbf6513b63345
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-08-04 00:54:21 +0530

    Counting Number Digits

--- src/qspeiderPrograms/numberPrograms/CountingNumDigits.java
+++ src/qspeiderPrograms/numberPrograms/CountingNumDigits.java
@@ -2,8 +2,44 @@ package qspeiderPrograms.numberPrograms;
 
 public class CountingNumDigits {
     public static void main(String[] args) {
-        int num=12345554;
-        int count=0;
+        int num = 12345;
+                int count = 0;
 
+                while (num != 0) {
+                    num = num / 10;
+                    count++;
+                }
+
+                System.out.println("Number of digits: " + count);
+            }
+        }
+    /*
+    public class CountDigits {
+
+    public static void main(String[] args) {
+        int num = 0;
+        int count = 0;
+
+        if (num == 0) {
+            count = 1;
+        } else {
+            while (num != 0) {
+                num /= 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits: " + count);
     }
 }
+--------------------Using String ----------
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 12345;
+
+        int count = String.valueOf(num).length();
+
+        System.out.println("Number of digits: " + count);
+    }
+}
+     */

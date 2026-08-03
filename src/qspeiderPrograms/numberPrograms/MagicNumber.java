0fd8e4e8f69135d62885a412c27c6e611250a6bd
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-08-04 01:04:13 +0530

    Magic Number

---
+++ src/qspeiderPrograms/numberPrograms/MagicNumber.java
@@ -0,0 +1,39 @@
+package qspeiderPrograms.numberPrograms;
+
+import java.util.Scanner;
+
+public class MagicNumber {
+        static int squareSum(int n) {
+
+            int sum = 0;
+
+            while (n > 0) {
+
+                int rem = n % 10;
+
+                sum += rem * rem;
+
+                n /= 10;
+            }
+
+            return sum;
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter Number : ");
+
+            int num = sc.nextInt();
+
+            while (num != 1 && num != 4) {
+                num = squareSum(num);
+            }
+
+            if (num == 1)
+                System.out.println("Happy Number");
+            else
+                System.out.println("Sad Number");
+        }
+    }

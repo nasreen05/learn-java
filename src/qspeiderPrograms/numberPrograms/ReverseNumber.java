227056e3e00423e9a071c69820bf7378a8490c3c
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-08-04 01:06:46 +0530

    Revese  Number

---
+++ src/qspeiderPrograms/numberPrograms/ReverseNumber.java
@@ -0,0 +1,16 @@
+package qspeiderPrograms.numberPrograms;
+
+public class ReverseNumber {
+       public static void main(String[] args) {
+            int num = 12345;
+            int reverse = 0;
+
+            while (num != 0) {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num /= 10;
+            }
+
+            System.out.println("Reversed number: " + reverse);
+        }
+    }
